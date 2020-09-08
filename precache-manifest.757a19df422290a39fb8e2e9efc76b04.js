8bd23f4b176954e27fd29a2b67f141460768fcbc
Nabeel Amjad <nabeel@bloomrix.com>
2020-09-08 16:49:23 +0500

    Updates

---
+++ precache-manifest.757a19df422290a39fb8e2e9efc76b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e0bc880217b5a7af638fcc16775eb4",
+    "url": "/firstreactapp/index.html"
+  },
+  {
+    "revision": "0a5d26a1416a70b71a3f",
+    "url": "/firstreactapp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7b126c356fb026b2c24f",
+    "url": "/firstreactapp/static/css/main.623932e4.chunk.css"
+  },
+  {
+    "revision": "0a5d26a1416a70b71a3f",
+    "url": "/firstreactapp/static/js/2.69156900.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/firstreactapp/static/js/2.69156900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b126c356fb026b2c24f",
+    "url": "/firstreactapp/static/js/main.a130d098.chunk.js"
+  },
+  {
+    "revision": "b60ce339e52b0a4c9c0c",
+    "url": "/firstreactapp/static/js/runtime-main.816502d0.js"
+  }
+]);
